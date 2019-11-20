232fc5265b1f82131feb0c95f4f2a1fe29e5d9cc
HaoSususudio <bluehao85@gmail.com>
2019-11-20 10:10:48 -0500

    add resource folder and .gitignore

--- SQLScripts/temp-space.sql
+++ SQLScripts/temp-space.sql
@@ -1,4 +1,29 @@
-CREATE OR REPLACE PROCEDURE get_all_reimbursements
+CREATE OR REPLACE PROCEDURE new_reimb_query
+AS
+BEGIN
+  CREATE OR REPLACE VIEW reimb_query AS
+  SELECT *
+  FROM ers_reimbursement;
+END;
+/
+
+
+CREATE OR REPLACE PROCEDURE get_all_reimbursements()
+AS
+BEGIN
+  CREATE OR REPLACE VIEW reimb_query AS
+  SELECT *
+  FROM ers_reimbursement;
+END;
+/
+
+ select field
+  into variable
+  FROM ers_reimbursement
+ where condition
+
+
+ CREATE OR REPLACE PROCEDURE get_all_reimbursements
 (
   out_cursor_reimb OUT SYS_REFCURSOR
 )
@@ -6,49 +31,58 @@ AS
 BEGIN
   OPEN out_cursor_reimb FOR
   SELECT  reimb.reimb_id, reimb.reimb_amount, reimb.reimb_submitted, reimb.reimb_resolved, reimb.reimb_description,
-    author.ers_users_id, author.user_first_name, author.user_last_name, author.user_email, a_role.user_role,
-    resolver.ers_users_id, resolver.user_first_name, resolver.user_last_name, resolver.user_email, r_role.user_role,
-    sta.reimb_status, rtype.reimb_type
+    reimb.reimb_author, reimb.reimb_resolver, sta.reimb_status, rtype.reimb_type
   FROM ers_reimbursement reimb
-  LEFT JOIN ers_users author ON (reimb.reimb_author = author.ers_users_id)
-  LEFT JOIN ers_user_roles a_role ON (author.user_role_id = a_role.ers_user_role_id)
-  LEFT JOIN ers_users resolver ON (reimb.reimb_resolver = resolver.ers_users_id)
-  LEFT JOIN ers_user_roles r_role ON (resolver.user_role_id = r_role.ers_user_role_id)
   LEFT JOIN ers_reimbursement_status sta ON (reimb.reimb_status_id = sta.reimb_status_id)
   LEFT JOIN ers_reimbursement_type rtype ON (reimb.reimb_type_id = rtype.reimb_type_id)
   ORDER BY reimb.reimb_id;
 END;
 /
-SET     SERVEROUTPUT ON
-DECLARE c_out sys_refcursor;
-  reimb_id number;
-  reimb_amount number;
-  reimb_submitted timestamp;
-  reimb_resolved timestamp;
-  reimb_description varchar2;
-  author_id number;
-  author_first_name varchar2;
-  author_last_name varchar2;
-  author_email varchar2; 
-  a_role varchar2;
-  resolver_id number;
-  resolver_first_name varchar2;
-  resolver_last_name varchar2;
-  resolver_email varchar2;
-  r_role varchar2;
-  reimb_status varchar2;
-  reimb_type varchar2;
+
+
+procedure proexempt (parID number)
+is 
+  vardescription tithedb.parcel.description%type;
+  varland tithedb.landuse.landuse_id%type;
+  varlanduse tithedb.landuse.landuse%type;
+  varparcel tithedb.parcel.parcel_id%type;
+begin  
+  select parcel_id,
+         description,
+         landuse
+  into   varparcel,
+         vardescription,
+         varlanduse
+  from   door
+  where  parcel_id = parID;
+
+  -- ... more stuff
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    NULL; -- Handle error here.
+  WHEN TOO_MANY_ROWS THEN
+    NULL; -- Handle error here (or use "AND ROWNUM = 1" in your query).
+END proexempt;
+/
+
+
+
+DECLARE
+  emp_num       NUMBER(6) := 120;
+  bonus         NUMBER(6) := 50;
+  emp_last_name VARCHAR2(25);
+  PROCEDURE raise_salary (emp_id IN NUMBER, amount IN NUMBER, 
+                          emp_name OUT VARCHAR2) IS
+    BEGIN
+      UPDATE employees SET salary =
+        salary + amount WHERE employee_id = emp_id;
+      SELECT last_name INTO emp_name
+        FROM employees
+       WHERE employee_id = emp_id;
+  END raise_salary;
 BEGIN
-    get_all_reimbursements(c_out);
-    LOOP 
-        FETCH c_out
-        INTO  reimb_id, reimb_amount, reimb_submitted, reimb_resolved, reimb_description,
-          author_id, author_first_name, author_last_name, author_email, a_role,
-          resolver_id, resolver_first_name, resolver_last_name, resolver_email, r_role,
-          reimb_status, reimb_type;
-        EXIT WHEN c_out%NOTFOUND;
-        DBMS_OUTPUT.PUT_LINE(reimb_id || ' ' || author_id || ' ' || resolver_id || ' ' || reimb_status);
-    END LOOP;
-  CLOSE c_out;
+  raise_salary(emp_num, bonus, emp_last_name);
+  DBMS_OUTPUT.PUT_LINE
+    ('Salary was updated for: ' || emp_last_name);
 END;
 /
